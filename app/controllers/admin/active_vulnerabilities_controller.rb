e7cf76b0d014a29f2ee6c12d2b741792d7987911
Luke Brewerton <luke@lukebrewerton.com>
2017-11-13 16:52:17 +0000

    Started Active Vulnerabilities edit page

--- app/controllers/admin/active_vulnerabilities_controller.rb
+++ app/controllers/admin/active_vulnerabilities_controller.rb
@@ -4,11 +4,30 @@ class Admin::ActiveVulnerabilitiesController < ApplicationController
     @activevulnerabilities = ActiveVulnerability.all
   end
 
+  def edit
+    @activevulnerabilities = ActiveVulnerability.find(params[:id])
+  end
+
+  def update
+    attributes = av_params.clone
+    if @activevulnerabilities.update(av_params)
+      flash[:notice] = t('admin.active_vuln.update.success')
+      respond_with :edit, :admin, @activevulnerability
+    else
+      flash[:warning] = @activevulnerabilities.errors.full_messages.uniq.join(', ')
+      respond_with :edit, :admin, :activevulnerability
+    end
+  end
+
   private def layout_by_resource
     if devise_controller?
       'layouts/devise'
     else
       'admin/layouts/admin'
     end
+  end
+
+  private def av_params
+    params.require(:activevulnerability).permit(:displayname, :vulncount, :active)
   end
 end
--- app/helpers/active_vulnerabilities_helper.rb
+++ app/helpers/active_vulnerabilities_helper.rb
@@ -1,2 +1,6 @@
 module ActiveVulnerabilitiesHelper
+
+  private def activevulnerability_params
+    params.require(:activevulberabilities).permit(:active)
+  end
 end
