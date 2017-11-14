3e922e3fa1b2df635fdb0d12a8a803a3e2124864
Luke Brewerton <luke@lukebrewerton.com>
2017-11-14 17:28:44 +0000

    Finished Vulnerabilities module

--- app/controllers/admin/vulnerabilities_controller.rb
+++ app/controllers/admin/vulnerabilities_controller.rb
@@ -1,9 +1,18 @@
 class Admin::VulnerabilitiesController < ApplicationController
   layout :layout_by_resource
+  before_action :set_vulnerability, only: [:show, :edit, :update, :destroy]
+
   def index
     @vulnerabilities = Vulnerability.all
   end
 
+  def show
+    @vulnerabilities = Vulnerability.find(params[:id])
+  end
+
+  private def set_vulnerability
+    @vulnerabilities = Vulnerability.find(params[:id])
+  end
   private def layout_by_resource
     if devise_controller?
       'layouts/devise'
