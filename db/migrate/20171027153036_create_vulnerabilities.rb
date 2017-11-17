bc780993b35c51f5e396c542e9d7e99e45b7a810
Luke Brewerton <luke@lukebrewerton.com>
2017-11-17 14:26:50 +0000

    Changed GEMFILE

--- db/migrate/20171027153036_create_vulnerabilities.rb
+++ db/migrate/20171027153036_create_vulnerabilities.rb
@@ -1,5 +1,5 @@
 class CreateDatabase < ActiveRecord::Migration
-  def self.up
+  def change
     create_table "vuln_sets", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
       t.string "vulntype", null: false
       t.string "displayname", null: false
@@ -9,9 +9,6 @@ class CreateDatabase < ActiveRecord::Migration
       t.timestamp "timestamp", default: -> { "CURRENT_TIMESTAMP" }, null: false
       t.index ["vulntype"], name: "vulntypeindex", unique: true
     end
-  def self.down
-
-  end
   end
 end
 
